9454a2dfc002d97e07318c0772e0a2e345ef06ef
Shikhar Bharadwaj <sbharad2@andrew.cmu.edu>
2025-04-09 13:47:53 -0400

    keep run simple

--- egs2/emo_news_sds/sds1/run.sh
+++ egs2/emo_news_sds/sds1/run.sh
@@ -8,8 +8,14 @@
     echo "ERROR: You need to setup the variable HF_TOKEN with your HuggingFace access token in local/path.sh" && \
 exit 1
 
+# python app.py \
+#   --eval_options "Latency,TTS Intelligibility,TTS Speech Quality,ASR WER,Text Dialog Metrics" \
+#   --tts_options "kan-bayashi/ljspeech_vits,kan-bayashi/libritts_xvector_vits,kan-bayashi/vctk_multi_spk_vits,ChatTTS" \
+#   --llm_options "meta-llama/Llama-3.2-1B-Instruct,HuggingFaceTB/SmolLM2-1.7B-Instruct" \
+#   --asr_options "pyf98/owsm_ctc_v3.1_1B,espnet/owsm_ctc_v3.2_ft_1B,espnet/owsm_v3.1_ebf,librispeech_asr,whisper-large"
+
 python app.py \
-  --eval_options "Latency,TTS Intelligibility,TTS Speech Quality,ASR WER,Text Dialog Metrics" \
-  --tts_options "kan-bayashi/ljspeech_vits,kan-bayashi/libritts_xvector_vits,kan-bayashi/vctk_multi_spk_vits,ChatTTS" \
-  --llm_options "meta-llama/Llama-3.2-1B-Instruct,HuggingFaceTB/SmolLM2-1.7B-Instruct" \
-  --asr_options "pyf98/owsm_ctc_v3.1_1B,espnet/owsm_ctc_v3.2_ft_1B,espnet/owsm_v3.1_ebf,librispeech_asr,whisper-large"
+  --eval_options "ASR WER" \
+  --tts_options "kan-bayashi/ljspeech_vits" \
+  --llm_options "HuggingFaceTB/SmolLM2-1.7B-Instruct" \
+  --asr_options "whisper-large"
